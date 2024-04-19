787469f8755f6d715be099a85736225f9ea5836f
ardonplay <ardonplay@gmail.com>
2024-04-19 12:52:46 +0300

    feat: add jwt parse uuid

--- src/main/java/io/github/semanticpie/pietunes/recommendation_service/config/SecurityConfiguration.java
+++ src/main/java/io/github/semanticpie/pietunes/recommendation_service/config/SecurityConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -23,7 +22,6 @@ public class SecurityConfiguration {
 
     @Value("${pie-tunes-ui.server.url}")
     private List<String> FRONTEND_URLs;
-
 
 
     @Bean
--- src/main/java/io/github/semanticpie/pietunes/recommendation_service/models/neo4j/UserNeo4j.java
+++ src/main/java/io/github/semanticpie/pietunes/recommendation_service/models/neo4j/UserNeo4j.java
@@ -7,8 +7,6 @@ import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Relationship;
 
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
